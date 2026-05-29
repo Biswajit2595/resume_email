d24182d32c6599aab5363e4ceebc272929cc8fbf
Biswajit Roy <broy16431@gmail.com>
2026-05-29 16:41:19 +0530

    health check point added

--- src/index.ts
+++ src/index.ts
@@ -9,6 +9,11 @@ app.get("/",(req,res)=>{
     res.send("AI resume analyser backend running 🚀")
 })
 
+app.get("/health",(req,res)=>{
+  res.status(201).json({"message":"Health is fine go ahead"})
+})
+
+
 
 app.listen(port, () => {
   logger.info(`Server listening on http://localhost:${port}`);
